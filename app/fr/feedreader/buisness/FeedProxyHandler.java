129cf0162ec75e99dd45953278dced3bbce9ed8d
FICHET Philippe <philippe@easyflirt.com>
2014-05-14 12:39:57 +0200

    Formattage du fichier

--- app/fr/feedreader/buisness/FeedProxyHandler.java
+++ app/fr/feedreader/buisness/FeedProxyHandler.java
@@ -20,113 +20,110 @@ import org.xml.sax.helpers.DefaultHandler;
 import fr.feedreader.models.FeedItem;
 
 public class FeedProxyHandler extends DefaultHandler {
-	
-	private FeedAbstract feedAbstractHandler = null;
-	
-	/**
+
+    private FeedAbstract feedAbstractHandler = null;
+
+    /**
      * Receive notification of the start of an element.
-     *
+     * <p/>
      * <p>By default, do nothing.  Application writers may override this
      * method in a subclass to take specific actions at the start of
      * each element (such as allocating a new tree node or writing
      * output to a file).</p>
      *
-     * @param uri The Namespace URI, or the empty string if the
-     *        element has no Namespace URI or if Namespace
-     *        processing is not being performed.
-     * @param localName The local name (without prefix), or the
-     *        empty string if Namespace processing is not being
-     *        performed.
-     * @param qName The qualified name (with prefix), or the
-     *        empty string if qualified names are not available.
+     * @param uri        The Namespace URI, or the empty string if the
+     *                   element has no Namespace URI or if Namespace
+     *                   processing is not being performed.
+     * @param localName  The local name (without prefix), or the
+     *                   empty string if Namespace processing is not being
+     *                   performed.
+     * @param qName      The qualified name (with prefix), or the
+     *                   empty string if qualified names are not available.
      * @param attributes The attributes attached to the element.  If
-     *        there are no attributes, it shall be an empty
-     *        Attributes object.
-     * @exception org.xml.sax.SAXException Any SAX exception, possibly
-     *            wrapping another exception.
+     *                   there are no attributes, it shall be an empty
+     *                   Attributes object.
+     * @throws org.xml.sax.SAXException Any SAX exception, possibly
+     *                                  wrapping another exception.
      * @see org.xml.sax.ContentHandler#startElement
      */
-    public void startElement (String uri, String localName,
-                              String qName, Attributes attributes)
-        throws SAXException
-    {
-    	if (qName.equalsIgnoreCase("rss") && feedAbstractHandler == null) {
-    		feedAbstractHandler = new FeedRss();
-    	} else if (qName.equalsIgnoreCase("feed") && feedAbstractHandler == null) {
-    		feedAbstractHandler = new FeedAtom();
-    	}
-    	
-    	if (feedAbstractHandler != null) {
-    		feedAbstractHandler.startElement(uri, localName, qName, attributes);
-    	}
+    public void startElement(String uri, String localName,
+                             String qName, Attributes attributes)
+            throws SAXException {
+        if (qName.equalsIgnoreCase("rss") && feedAbstractHandler == null) {
+            feedAbstractHandler = new FeedRss();
+        } else if (qName.equalsIgnoreCase("feed") && feedAbstractHandler == null) {
+            feedAbstractHandler = new FeedAtom();
+        }
+
+        if (feedAbstractHandler != null) {
+            feedAbstractHandler.startElement(uri, localName, qName, attributes);
+        }
     }
 
     /**
      * Receive notification of character data inside an element.
-     *
+     * <p/>
      * <p>By default, do nothing.  Application writers may override this
      * method to take specific actions for each chunk of character data
      * (such as adding the data to a node or buffer, or printing it to
      * a file).</p>
      *
-     * @param ch The characters.
-     * @param start The start position in the character array.
+     * @param ch     The characters.
+     * @param start  The start position in the character array.
      * @param length The number of characters to use from the
      *               character array.
-     * @exception org.xml.sax.SAXException Any SAX exception, possibly
-     *            wrapping another exception.
+     * @throws org.xml.sax.SAXException Any SAX exception, possibly
+     *                                  wrapping another exception.
      * @see org.xml.sax.ContentHandler#characters
      */
-    public void characters (char ch[], int start, int length)
-        throws SAXException
-    {
-    	if (feedAbstractHandler != null) {
-    		feedAbstractHandler.characters(ch, start, length);
-    	}
+    public void characters(char ch[], int start, int length)
+            throws SAXException {
+        if (feedAbstractHandler != null) {
+            feedAbstractHandler.characters(ch, start, length);
+        }
     }
-    
+
     /**
      * Receive notification of the end of an element.
-     *
+     * <p/>
      * <p>By default, do nothing.  Application writers may override this
      * method in a subclass to take specific actions at the end of
      * each element (such as finalising a tree node or writing
      * output to a file).</p>
      *
-     * @param uri The Namespace URI, or the empty string if the
-     *        element has no Namespace URI or if Namespace
-     *        processing is not being performed.
+     * @param uri       The Namespace URI, or the empty string if the
+     *                  element has no Namespace URI or if Namespace
+     *                  processing is not being performed.
      * @param localName The local name (without prefix), or the
-     *        empty string if Namespace processing is not being
-     *        performed.
-     * @param qName The qualified name (with prefix), or the
-     *        empty string if qualified names are not available.
-     * @exception org.xml.sax.SAXException Any SAX exception, possibly
-     *            wrapping another exception.
+     *                  empty string if Namespace processing is not being
+     *                  performed.
+     * @param qName     The qualified name (with prefix), or the
+     *                  empty string if qualified names are not available.
+     * @throws org.xml.sax.SAXException Any SAX exception, possibly
+     *                                  wrapping another exception.
      * @see org.xml.sax.ContentHandler#endElement
      */
-    public void endElement (String uri, String localName, String qName)
-        throws SAXException
-    {
-    	if (feedAbstractHandler != null) {
-    		feedAbstractHandler.endElement(uri, localName, qName);
-    	}
+    public void endElement(String uri, String localName, String qName)
+            throws SAXException {
+        if (feedAbstractHandler != null) {
+            feedAbstractHandler.endElement(uri, localName, qName);
+        }
     }
-    
-    
+
+
     public List<FeedItem> getFeedItems() {
-    	if (feedAbstractHandler != null) {
-    		return feedAbstractHandler.getFeedItems();
-    	} else {
-    		return new ArrayList<FeedItem>();
-    	}
+        if (feedAbstractHandler != null) {
+            return feedAbstractHandler.getFeedItems();
+        } else {
+            return new ArrayList<FeedItem>();
+        }
     }
-    
+
     public String getType() {
-    	if (feedAbstractHandler != null) {
-    		return feedAbstractHandler.getType();
-    	} else {
-    		return null;
-    	}
+        if (feedAbstractHandler != null) {
+            return feedAbstractHandler.getType();
+        } else {
+            return null;
+        }
     }
 }
